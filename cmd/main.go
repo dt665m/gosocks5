1991c42de7b2b23f629cfba6651e6eccd6895b33
Denis Tsai <denistsai@blazingorb.com>
2018-04-19 03:19:39 -0700

    example modified

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,18 @@ import (
 
 	"github.com/dt665m/gosocks5"
 )
+
+var (
+	port     string
+	login    string
+	password string
+)
+
+func init() {
+	flag.StringVar(&port, "p", "8080", "socks5 listen port")
+	flag.StringVar(&login, "user", "", "socks5 username")
+	flag.StringVar(&password, "password", "", "socks5 password")
+}
 
 func main() {
 	go func() {
@@ -20,15 +33,19 @@ func main() {
 		http.ListenAndServe(":8888", mux)
 	}()
 	conf := &socks5.Config{}
-	conf.Credentials = socks5.StaticCredentials(map[string]string{"hello": "world"})
 	conf.Logger = log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
+	if login != "" && password != "" {
+		conf.Credentials = socks5.StaticCredentials(map[string]string{login: password})
+	} else {
+		conf.Logger.Println("WARN: no authentication provided (free use mode)")
+	}
 	server, err := socks5.New(conf)
 	if err != nil {
 		panic(err)
 	}
 
 	// Create SOCKS5 proxy on localhost port 8080
-	fmt.Println("Starting SOCKS5 on 8080")
+	conf.Logger.Println("starting SOCKS5, port 8080")
 	if err := server.ListenAndServe("tcp", ":8080"); err != nil {
 		panic(err)
 	}
