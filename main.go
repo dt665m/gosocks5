c5752e19beb88136fea66f48aa0939693297c17e
Denis Tsai <denistsai@blazingorb.com>
2018-04-19 02:33:02 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/armon/go-socks5"
+)
+
+func main() {
+	conf := &socks5.Config{}
+	conf.Credentials = socks5.StaticCredentials(map[string]string{"hello": "world"})
+	conf.Logger = log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
+	server, err := socks5.New(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create SOCKS5 proxy on localhost port 8000
+	if err := server.ListenAndServe("tcp", ":8080"); err != nil {
+		panic(err)
+	}
+}
