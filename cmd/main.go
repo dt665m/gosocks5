5ee9438e7922bbf68d8a524ba334c7691865ce5b
Denis Tsai <denistsai@blazingorb.com>
2018-04-19 02:56:13 -0700

    example updated

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/dt665m/gosocks5"
 )
 
 func main() {
+	go func() {
+		fmt.Println("Starting Test Server 8888")
+		mux := http.NewServeMux()
+		mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+			ip := r.RemoteAddr
+			fmt.Fprintf(w, "your ip is %v", ip)
+		})
+		http.ListenAndServe(":8888", mux)
+	}()
 	conf := &socks5.Config{}
 	conf.Credentials = socks5.StaticCredentials(map[string]string{"hello": "world"})
 	conf.Logger = log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
@@ -16,7 +27,8 @@ func main() {
 		panic(err)
 	}
 
-	// Create SOCKS5 proxy on localhost port 8000
+	// Create SOCKS5 proxy on localhost port 8080
+	fmt.Println("Starting SOCKS5 on 8080")
 	if err := server.ListenAndServe("tcp", ":8080"); err != nil {
 		panic(err)
 	}
